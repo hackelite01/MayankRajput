3f68fe41a7427ca9031c1742a7f50aa4f94503fd
Mayank Rajput <78653024+hackelite01@users.noreply.github.com>
2024-10-02 22:11:13 +0530

    Achievements

---
+++ src/components/styles/Achievements.styled.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const AchIntro = styled.h2`
+  color: #3498db;
+  margin-bottom: 20px;
+`;
+
+export const AchList = styled.ul`
+  list-style-type: disc;
+  padding-left: 20px;
+`;
+
+export const AchItem = styled.li`
+  margin-bottom: 10px;
+  .title {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+`;
